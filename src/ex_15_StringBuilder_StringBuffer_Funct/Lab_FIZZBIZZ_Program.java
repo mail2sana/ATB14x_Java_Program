470cd2e8955c3225b2d3af48e83eb5c3194c5a84
SANGEETHA <mail2sana.v@gmail.com>
2025-10-09 12:03:05 +0530

    5-Oct-25 Part1 - Ex-15 : String Builder & String Buffer

---
+++ src/ex_15_StringBuilder_StringBuffer_Funct/Lab_FIZZBIZZ_Program.java
@@ -0,0 +1,23 @@
+package ex_15_StringBuilder_StringBuffer_Funct;
+
+public class Lab_FIZZBIZZ_Program {
+    /* Write a program that prints numbers from 1 to 100.
+    However, for multiples of 3, print "Fizz" instead of the number,
+    and for multiples of 5, print "Buzz."
+    For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <=100 ; i++) {
+            System.out.println(i);
+          if(i%3==0){
+                System.out.println("Fizz");
+                continue;
+            } else if (i%5==0) {
+                System.out.println("Buzz");
+                continue;
+            } else {
+                System.out.println("FizzBuzz");
+            }
+        }
+    }
+}
---
+++ src/ex_15_StringBuilder_StringBuffer_Funct/Lab_Grade_Calculator_Program.java
@@ -0,0 +1,38 @@
+package ex_15_StringBuilder_StringBuffer_Funct;
+
+import java.util.Scanner;
+
+public class Lab_Grade_Calculator_Program {
+    /*  Write a program that calculates and displays the letter grade for a given numerical score
+     (e.g., A, B, C, D, or F) based on the following grading scale:
+A: 90-100
+B: 80-89
+C: 70-79
+D: 60-69
+F: 0-59
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your score pls");
+        int score = scanner.nextInt();
+        if (score>=90 && score<100) {
+            System.out.println("Congratulation youe are into Grade A");
+        }
+        else if (score>=80 && score<89) {
+            System.out.println("Good Grade B");
+
+        }
+
+        else if (score>=70 && score<79) {
+            System.out.println("Improve Grade C");
+
+        }
+        else if (score>= 60 && score<69) {
+            System.out.println("Normal Grade D");
+
+        }
+        else {
+            System.out.println("Need to Improve Grade F");
+        }
+    }
+}
